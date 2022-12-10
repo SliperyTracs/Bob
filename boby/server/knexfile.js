1ad030d818e69fbc9d02cf12c9c9c300aaccb0ee
sliperytracs <shoujun032@gmail.com>
2022-12-10 22:24:31 +0800

    able to talk to data base in mysql

---
+++ boby/server/knexfile.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'T0327587b!',
+      database: 'boby_db',
+    },
+    migrations: {
+      directory: path.join(__dirname, '/migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, '/seeds'),
+    },
+  },
+};
